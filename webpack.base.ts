d3b8642f47e2111fad33c4336934a593a6779b46
CatalystDP <469589860@qq.com>
2019-01-30 23:31:48 +0800

    first commit

---
+++ webpack.base.ts
@@ -0,0 +1,13 @@
+import webpack = require("webpack");
+
+let a: string = 'fdafdsa';
+
+
+console.log(a);
+
+// let baseConfig: webpack.Configuration = {
+//     entry:{
+//        app:'./src/main.tsx' 
+//     }
+// };
+// export default baseConfig;
